49ea82bf77965e2ccf592839e803b2ce9c892f8c
lama <lama@odoo.com>
2023-08-03 01:41:47 +0300

    Clean Architecture design

---
+++ lib/currencyConversion/domain/entities/currency.dart
@@ -0,0 +1,11 @@
+class Currency{
+
+  // currency class properities
+  final double currencyRate;
+  final String currencyFlag;
+  final String currencyName;
+
+  // constructor
+  Currency({required this.currencyRate, required this.currencyFlag, required this.currencyName});
+
+}
